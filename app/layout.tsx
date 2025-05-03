99421a2ca907ba5d74099e8b1d4cd74be03ae199
Sajjad Abedi <sajadxabedi@gmail.com>
2025-05-03 23:38:57 +0300

    fix: bg color fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="caret-cyan-400 antialiased">
+    <html lang="en" className="caret-cyan-400 antialiased dark:bg-neutral-900">
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function FeedbackComponentCSS() {
   }, [open, formState])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 dark:bg-neutral-900">
       <div className="h-[500px] flex justify-center items-center w-full">
         <FeedbackButton
           onClick={() => {
